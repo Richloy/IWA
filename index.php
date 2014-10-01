e4f7893cfd54c19c52a7c438045c630d23ca4486
Richloy <richloy@gmail.com>
2014-10-01 19:21:21 +0100

    Create index.php
    IWA Lab 1

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+
+	<head>
+
+ 	 <meta charset="utf-8">
+ 	 <title>Login Form IWA Lab1</title>
+ 	 <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
+
+	</head>
+
+
+
+	<form>
+
+ 	   Username: <input type="text" name="username" id="username"></input><br>
+	    Password: <input type="password" name="pw" id="pw"> </input><br>
+
+ 	   <button onclick="getValuesFromData();">Test</button>
+
+	</form>
+
+
+	<script>
+
+		function getValuesFromData(){
+
+		var username = document.getElementById('username').value;
+		var password = document.getElementById('pw').value;
+
+  		  $.post( "pageb.php", { username: username, password: password }).done(function( data ) {
+  		    	alert( "Data Loaded: " + data );
+   		  });
+		}
+
+	</script>
+
+</html>
